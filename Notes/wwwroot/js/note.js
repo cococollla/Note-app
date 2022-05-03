a7f0cf50ac611551dcd6bde1a2b4f4c1fb207660
torontotoky <nickolai.bel1aev@yandex.ru>
2022-05-03 22:40:59 +0300

    Добавил тул бар для текста, сделать функционал этих кнопок

--- Notes/wwwroot/js/note.js
+++ Notes/wwwroot/js/note.js
@@ -1,18 +1,33 @@
 ﻿function openForm(e, nameAction) {
     $('#modelWindowForm').addClass('active');
+
     let parent = $(e).parent().prev();
     console.log(parent)
+
     let id = parent.find('#id').val();
     console.log(id)
+
     let name = parent.find('#name').text().trim();
     console.log(name);
+
     let discription = parent.find('#discription').text().trim();
     console.log(discription)
+
     if (nameAction !== "Note/CreateNote") {
         $("input[name='Id']").val(id);
         $("input[name='Name']").val(name);
-        $("input[name='Discription']").val(discription);
+        $("textarea[name='Discription']").val(discription);
+    }
+    else {
+        $("input[name='Name']").val('');
+        $("textarea[name='Discription']").val('');
     }
 
     $("#noteForm").attr("action", nameAction);
 }
+
+function resizeTxta(element) {
+    element.style.height = "auto";
+    element.style.height = (element.scrollHeight) + "px";
+}
+
